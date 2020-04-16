9c1f8b5660abeb229572875f7eeb7a8d618a3dbc
Abhishek Anshu <abhianshu2020@gmail.com>
2020-04-16 20:56:50 +0530

    Predictor Service and Prediction Modal Implemented

---
+++ src/app/services/health-predictor.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HealthPredictorService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getHeartPredictorResult(data) {
+    return of({Result: 0, Status: 200});
+    //return this.httpClient.post('http://15.206.14.87:5000/predict', data);
+  }
+}
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, Validators } from '@angular/forms';
+import { HealthPredictorService } from '../services/health-predictor.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -11,26 +13,67 @@ export class UserProfileComponent implements OnInit {
   title = 'appBootstrap';
   
   closeResult: string;
-  
-  constructor(private modalService: NgbModal) {}
-    
-  open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+  items;
+  checkoutForm; 
+  submitted = false;
+  isHeartOK: Boolean;
+
+
+  // @ViewChild('dialog', {static: false}) resultDialog;
+  @ViewChild('mymodal') resultDialog;
+
+  constructor(private modalService: NgbModal, private formBuilder: FormBuilder,
+     private healthPredictor: HealthPredictorService) {
+    this.checkoutForm = this.formBuilder.group({
+      age: ['', Validators.required],
+      sex: ['', Validators.required],
+      cp: ['', Validators.required],
+      trestbps: ['', Validators.required],
+      chol: ['', Validators.required],
+      fbs: ['', Validators.required],
+      restecg: ['', Validators.required],
+      thalach: ['', Validators.required],
+      exang: ['', Validators.required],
+      oldpeak: ['', Validators.required],
+      slope: ['', Validators.required],
+      ca: ['', Validators.required],
+      thal: ['', Validators.required],
     });
   }
-  
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log('yo')
+     this.healthPredictor.getHeartPredictorResult(this.checkoutForm.value).subscribe((response: any) => {      
+      if (response.Status === 200) {
+        this.modalService.open(this.resultDialog);
+        this.isHeartOK = !!response.Result;
+        }
+    });
+
+    //this.checkoutForm.reset();
+   
+    
   }
+
+    
+  // open(content) {
+  //   this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+  //     this.closeResult = `Closed with: ${result}`;
+  //   }, (reason) => {
+  //     this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+  //   });
+  // }
+  
+  // private getDismissReason(reason: any): string {
+  //   if (reason === ModalDismissReasons.ESC) {
+  //     return 'by pressing ESC';
+  //   } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+  //     return 'by clicking on a backdrop';
+  //   } else {
+  //     return  `with: ${reason}`;
+  //   }
+  // }
 
   ngOnInit() {
   }
